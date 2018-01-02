7ac1c8549d5b97e7331d8af5348bed5142a60319
Matt Farejowicz <mfarejowicz@gmail.com>
2018-01-01 19:02:02 -0600

    work on new page

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express()
 app.use(express.static('templates'))
 app.use('/static', express.static('static'))
 
+// 404
+app.get('*', function(req, res) {
+  res.send('oops 404')
+})
+
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
--- static/main.js
+++ static/main.js
@@ -4,4 +4,6 @@ var h = d.getHours();
 
 var currentTime = h+":"+m;
 var timeControl = document.querySelector('input[type="time"]');
-timeControl.value = currentTime;
+if (timeControl) {
+  timeControl.value = currentTime;
+}
