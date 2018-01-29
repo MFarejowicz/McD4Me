f587da55b536d54f616c2d1c93cf8379baf282af
Matt Farejowicz <mfarejowicz@gmail.com>
2018-01-28 19:52:12 -0600

    reorganize a bit

--- public/static/app.js
+++ public/static/app.js
@@ -11,76 +11,93 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
-  // var ref = firebase.database().ref();
-  // var testRef = ref.child("test");
-  //
-  // const roomSpan = document.getElementById("roomNum");
-  // const timeSpan = document.getElementById("timeLeft");
-  // const orderList = document.getElementById("orderList");
-  //
-  // testRef.on("value", function(snap) {
-  //   console.log(snap.val());
-  //
-  //   if (roomSpan) {
-  //     roomSpan.innerText = "test";
-  //   }
-  //
-  //   if (timeSpan) {
-  //     var date = new Date(null);
-  //     date.setSeconds(snap.val().time);
-  //     var result = date.toISOString().substr(11, 8);
-  //     timeSpan.innerText = result;
-  //   }
-  //
-  //   for (var order in snap.val().orders) {
-  //     console.log(snap.val().orders[order]);
-  //   }
-  //
-  // });
-
   var url = "./static/menus/mcd.json";
 
-  $.getJSON(url, order);
+  $.getJSON(url, doActions);
 
-  function order(data) {
-    var order = {};
-    order.items = [];
-    order.cost = 0;
+  function doActions(data) {
+    makeMenu(data);
+    handleToggle();
+    takeOrder();
+  }
+
+  function makeMenu(data){
     var menuItems = data;
-    for (var key in menuItems) {
-      // console.log(key);
-      $("#menu").append("<p>"+key+"</p>");
-      // console.log(menuItems[key]);
-      if (menuItems[key].length > 0) {
-        for (var item of menuItems[key]) {
-          // console.log(item);
-          $("#menu").append("<div class='menu-item'><input id='" + item.id + "' type='checkbox'></input>" + "<label for='" + item.id + "'>$" + item.price + " - " + item.name + "</label></div>");
-          $("#" + item.id).data(item);
-        }
-      } else {
-        $("#menu").append("<div> No items here </div>");
+    var groups = [];
+    for (var item of menuItems) {
+      if (!(groups.includes(item.group))) {
+        groups.push(item.group);
+        $("#menu").append("<div><p data-group='" + item.group + "' class='menu-group-title'>" + item.group + "</p><div data-group='" + item.group + "' style='display: none'></div></div>");
       }
+      $("div[data-group='" + item.group + "']").append("<div class='menu-item'><input id='" + item.id + "' type='checkbox'></input><label for='" + item.id + "'>$" + item.price + " - " + item.name + "</label></div>");
+      $("#" + item.id).data(item);
     }
-    $("input").click(function() {
-      // console.log(this);
-      var item = $("#" + this.id).data();
-      // console.log(item);
-      if ($('#' + this.id).is(":checked")) {
-        $("#order").append("<div id=" + this.id + " class='menu-item'><span>$" + item.price + " - " + item.name + "</span></div>");
-        order.items.push(this.id);
-        order.cost += item.price;
-        // console.log(order);
-      } else {
-        $("div#" + this.id).remove();
-        order.items = order.items.filter(item => item !== this.id);
-        order.cost -= item.price;
-        // console.log(order);
-      }
+  }
+
+  function handleToggle() {
+    $("p[data-group]").click(function(){
+      var group = $(this).attr("data-group");
+      $("div[data-group='" + group + "']").toggle(500);
     });
   }
 
+  function takeOrder() {
+    var order = {};
+    order.items = [];
+    order.cost = 0;
+    $("input[type='checkbox'").click(function() {
+      var el = this;
+      var selector = $("#" + el.id);
+      var itemData = selector.data();
+      if (selector.is(":checked")) {
+        $("#order").append("<div class='order-item' id='" + itemData.id + "'><div class='order-item-top'><span>$" + itemData.price + " - " + itemData.name +
+          "</span><div class='quantity-div'><label for='" + itemData.id + "-amt'>Quantity: </label><input type='number' id='" +
+          itemData.id + "-amt' class='quantity-input' value='1' min='1'></input></div></div><div><span>Special Instructions:</span><textarea class='instr-text'></textarea></div></div>");
+        order.items.push(itemData.id);
+        order.cost += itemData.price;
+        order.cost = round(order.cost);
+      } else {
+        $("div#" + itemData.id).remove();
+        order.items = order.items.filter(item => item !== itemData.id);
+        order.cost -= itemData.price;
+        order.cost = round(order.cost);
+      }
+      console.log(order);
+    });
+  }
+
+  function round(value) {
+    return parseFloat(value.toFixed(2));
+  }
 
 });
+
+// var ref = firebase.database().ref();
+// var testRef = ref.child("test");
+//
+// const roomSpan = document.getElementById("roomNum");
+// const timeSpan = document.getElementById("timeLeft");
+// const orderList = document.getElementById("orderList");
+//
+// testRef.on("value", function(snap) {
+//   console.log(snap.val());
+//
+//   if (roomSpan) {
+//     roomSpan.innerText = "test";
+//   }
+//
+//   if (timeSpan) {
+//     var date = new Date(null);
+//     date.setSeconds(snap.val().time);
+//     var result = date.toISOString().substr(11, 8);
+//     timeSpan.innerText = result;
+//   }
+//
+//   for (var order in snap.val().orders) {
+//     console.log(snap.val().orders[order]);
+//   }
+//
+// });
 
 
 // const express = require('express')
