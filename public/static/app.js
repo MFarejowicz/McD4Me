02117634347c3a3fe445a0aa75c52019a6b55824
Matt Farejowicz <mfarejowicz@gmail.com>
2018-02-02 19:32:05 -0600

    more work better

--- public/static/app.js
+++ public/static/app.js
@@ -11,6 +11,70 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
+  class Order {
+    constructor() {
+      this.name = "";
+      this.items = [];
+      this.subTotal = 0;
+      this.taxTip = 0;
+      this.total = 0;
+    }
+    getItems() {
+      return this.items;
+    }
+    getCosts() {
+      return {subTotal: this.subTotal, taxTip: this.taxTip, total: this.total};
+    }
+    addItem(id, name, price, quantity, instructions) {
+      var newItem = {}
+      newItem.id = id;
+      newItem.name = name;
+      newItem.price = price;
+      newItem.quantity = quantity;
+      newItem.itemCost = price * quantity;
+      newItem.instructions = instructions;
+      this.items.push(newItem)
+      this.calculateCosts();
+    }
+    removeItem(id) {
+      this.items = this.items.filter(item => item.id != id);
+      this.calculateCosts();
+    }
+    changeQuantity(id, quantity) {
+      var index = 0;
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].id == id) {
+          index = i;
+          break;
+        }
+      }
+      if (quantity > 0 && quantity != this.items[index].quantity) {
+        this.items[index].quantity = quantity;
+        this.items[index].itemCost = this.items[index].price * this.items[index].quantity
+        this.calculateCosts();
+      }
+    }
+    changeInstructions(id, instructions) {
+      var index = 0;
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].id == id) {
+          index = i;
+          break;
+        }
+      }
+      this.items[index].instructions = instructions;
+    }
+    calculateCosts() {
+      var subTotal = 0;
+      for (var item of this.items) {
+        subTotal += item.price * item.quantity;
+      }
+      this.subTotal = round(subTotal);
+      this.taxTip = round(0.15 * this.subTotal);
+      this.total = round(this.subTotal + this.taxTip);
+    }
+  }
+
   var url = "./static/menus/mcd.json";
 
   $.getJSON(url, doActions);
@@ -42,46 +106,47 @@ $(document).ready(function(){
   }
 
   function takeOrder() {
-    var order = {};
-    order.items = [];
-    order.totalCost = 0;
+    var order = new Order();
     $("input[type='checkbox'").click(function() {
       var el = this;
       var selector = $("#" + el.id);
       var itemData = selector.data();
       if (selector.is(":checked")) {
         $("#order").append("<div class='order-item' id='" + itemData.id + "'><div class='order-item-top'><span>$" + itemData.price + " - " + itemData.name +
-          "</span><div class='quantity-div'><label for='" + itemData.id + "-amt'>Quantity: </label><input type='number' id='" +
-          itemData.id + "-amt' class='quantity-input' value='1' min='1'></input></div></div><div><span>Special Instructions:</span><textarea class='instr-text'></textarea></div></div>");
-        var newItem = {};
-        newItem[itemData.id] = {name: itemData.name, cost: itemData.price, quantity: 1, instructions: ""};
-        order.items.push(newItem);
-        // order.totalCost += itemData.price;
-        // order.totalCost = round(order.totalCost);
+          "</span><div class='remove-item' data-rem='" + itemData.id + "'><span>X</span></div></div><div class='quantity-div'><label for='" + itemData.id + "-amt'>Quantity: </label><input type='number' id='" +
+          itemData.id + "-amt' data-amt='" + itemData.id + "' class='quantity-input' value='1' min='1'></input></div><div><span>Special Instructions:</span><textarea data-instr='" + itemData.id + "' class='instr-text'></textarea></div></div>");
+        order.addItem(itemData.id, itemData.name, itemData.price, 1, "");
       } else {
         $("div#" + itemData.id).remove();
-        order.items = order.items.filter(item => item !== itemData.id);
-        // order.totalCost -= itemData.price;
-        // order.totalCost = round(order.totalCost);
+        order.removeItem(itemData.id);
       }
-      // console.log(order);
-      $(".quantity-input").bind('keyup mouseup', function () {
-        var id = this.id;
-        id = id.replace("-amt", "");
-        var ind;
-        for (var el in order.items){
-          if (id == Object.keys(order.items[el])[0]) {
-            ind = el;
-            break;
-          }
-        }
-        var val = parseInt($(this).val());
-        if (val > 0 && val != order.items[ind][id].quantity){
-          order.items[ind][id].quantity = val;
-        }
-        console.log(order);
+      updateCosts(order);
+      $(".quantity-input").off().bind('keyup mouseup', function () {
+        var elID = $(this).attr("data-amt");
+        var amt = parseInt($(this).val());
+        order.changeQuantity(elID, amt);
+        updateCosts(order);
+      });
+      $(".instr-text").off().bind('keyup mouseup', function () {
+        var elID = $(this).attr("data-instr");
+        var instructions = $(this).val();
+        order.changeInstructions(elID, instructions);
+      });
+      $(".remove-item").off().click(function() {
+        var elID = $(this).attr("data-rem");
+        $("div#" + elID).remove();
+        $('#' + elID).prop('checked', false);
+        order.removeItem(elID);
+        updateCosts(order);
       });
     });
+  }
+
+  function updateCosts(order) {
+    var costs = order.getCosts();
+    $("#subtotal").text("$" + costs.subTotal);
+    $("#taxtip").text("$" + costs.taxTip);
+    $("#total").text("$" + costs.total);
   }
 
 
